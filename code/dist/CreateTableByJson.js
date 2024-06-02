49556659f07167b3b5775760ff889801efbf3e0f
Everton Cezar do Prado Junior <ecpj.prado@gmail.com>
2024-06-02 19:58:51 -0300

    type tests
    table type tests

--- code/dist/CreateTableByJson.js
+++ code/dist/CreateTableByJson.js
@@ -1,87 +1,45 @@
 "use strict";
-function getArea(shape) {
-    switch (shape.kind) {
-        case "circle":
-            return Math.PI * shape.radius ** 2;
-        case "square":
-            return shape.sideLenght ** 2;
-        case "triangle":
-            return shape.sideLenght ** 2;
-        default:
-            const _exhaustiveCheck = shape;
-            return _exhaustiveCheck;
+const html_table = document.createElement('table');
+const html_table_title = document.createElement('title');
+const html_table_caption = document.createElement('caption');
+const html_thead = document.createElement('thead');
+const html_tbody = document.createElement('tbody');
+const html_tfoot = document.createElement('tfoot');
+const html_tr = document.createElement('tr');
+const html_th = document.createElement('th');
+const html_td = document.createElement('td');
+function createTdRow(str) {
+    const tr = document.createElement('tr');
+    for (let index = 0; index < 7; index++) {
+        const td = document.createElement(str);
+        td.innerText = "Iteração: " + index;
+        tr.appendChild(td);
+    }
+    return tr;
+}
+for (let index = 0; index < 25; index++) {
+    html_tbody.appendChild(createTdRow('td'));
+}
+html_thead.append(createTdRow('th'));
+html_tfoot.append(createTdRow('th'));
+html_table_caption.innerText = "Teste de Descrição de Tabela";
+html_table.appendChild(html_table_caption);
+html_table.appendChild(html_thead);
+html_table.appendChild(html_tbody);
+html_table.appendChild(html_tfoot);
+document.body.appendChild(html_table);
+[];
+class CreateTableByJson {
+    table;
+    data;
+    headers;
+    constructor(data, headers, table) {
+        this.table = table ? table : document.createElement('table');
+        this.data = data;
+        this.headers = headers;
     }
 }
-let a = { kind: "circle", radius: 22 };
-console.log(getArea(a));
-function printToConsole(s) {
-    console.log(s);
-}
-greeter(printToConsole);
-function greeter(fn) {
-    fn("Hello World");
-}
-function doSomething(fn) {
-    console.log(fn.description + " returned" + fn(6));
-}
-function myFunc(someArg) {
-    return someArg > 3;
-}
-myFunc.description = "default description";
-doSomething(myFunc);
-class SomeObject {
-    some;
-    constructor(s) {
-        this.some = s;
-    }
-}
-function fn(ctor) {
-    return new ctor("hello");
-}
-let x = SomeObject;
-console.log(fn(x).some);
-function firstElement(arr) {
-    return arr[0];
-}
-const s = firstElement(["a", "b", "c"]);
-const n = firstElement([1, 2, 3]);
-const u = firstElement([]);
-console.log(s, n, u);
-function map(arr, func) {
-    return arr.map(func);
-}
-const parsed = map(["1", "2", "3"], (n) => parseInt(n));
-console.log(parsed);
-function longest(a, b) {
-    if (a.length >= b.length) {
-        return a;
-    }
-    else {
-        return b;
-    }
-}
-const longerArray = longest([1, 2], [1, 2, 3]);
-const longerString = longest("alice", "bob");
-function myForEach(arr, callback) {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i], i);
-    }
-}
-myForEach([1, 2, 3], (a) => console.log(a));
-myForEach([1, 2, 3], (a, i) => console.log(a, i));
-function makeDate(mOrTimestamp, d, y) {
-    if (d !== undefined && y !== undefined) {
-        return new Date(y, mOrTimestamp, d);
-    }
-    else {
-        return new Date(mOrTimestamp);
-    }
-}
-const d1 = makeDate(12345678);
-const d2 = makeDate(5, 5, 5);
-console.log(d1, d2);
-function multiply(n, ...m) {
-    return m.map((x) => n * x);
-}
-const mult = multiply(10, 2, 3, 7);
-console.log(mult);
+const data_c1 = { data: "key", item: 25, teste: { kest: "Teste", id: 75 }, date: "2024-11-05" };
+const headers_c1 = [{ 'data': "Chave de Valores" }, { 'item.kest': "Teste" }, { 'date': 'Data' }];
+const c1 = new CreateTableByJson(data_c1, headers_c1, html_table);
+console.log(c1);
--- code/src/CreateTableByJson.ts
+++ code/src/CreateTableByJson.ts
@@ -1,151 +1,60 @@
-interface Circle {
-    kind: "circle";
-    radius: number;
+const html_table: HTMLTableElement = document.createElement('table');
+const html_table_title: HTMLTitleElement = document.createElement('title');
+const html_table_caption: HTMLTableCaptionElement = document.createElement('caption');
+
+const html_thead: HTMLTableSectionElement = document.createElement('thead');
+const html_tbody: HTMLTableSectionElement = document.createElement('tbody');
+const html_tfoot: HTMLTableSectionElement = document.createElement('tfoot');
+
+const html_tr: HTMLTableRowElement = document.createElement('tr');
+const html_th: HTMLTableCellElement = document.createElement('th');
+const html_td: HTMLTableCellElement = document.createElement('td');
+
+function createTdRow(str: "th" | "td"): HTMLTableRowElement {
+    const tr = document.createElement('tr');
+    for (let index = 0; index < 7; index++) {
+        const td = document.createElement(str);
+        td.innerText = "Iteração: " + index;
+        tr.appendChild(td);
+    }
+    return tr;
 }
 
-interface Square {
-    kind: "square";
-    sideLenght: number;
+for (let index = 0; index < 25; index++) {
+    html_tbody.appendChild(createTdRow('td'));
 }
 
-interface Triangle {
-    kind: "triangle";
-    sideLenght: number;
-}
+html_thead.append(createTdRow('th'));
+html_tfoot.append(createTdRow('th'));
 
-type Shape = Circle | Square | Triangle; 
+html_table_caption.innerText = "Teste de Descrição de Tabela";
 
-function getArea(shape: Shape): number {
-    switch (shape.kind) {
-        case "circle":
-            return Math.PI * shape.radius ** 2;
-        case "square":
-            return shape.sideLenght ** 2;
-        case "triangle":
-            return shape.sideLenght **2;
-        default:
-            const _exhaustiveCheck: never = shape;
-            return _exhaustiveCheck;
+html_table.appendChild(html_table_caption);
+html_table.appendChild(html_thead);
+html_table.appendChild(html_tbody);
+html_table.appendChild(html_tfoot);
+
+document.body.appendChild(html_table);
+
+//type DataHeaders = { [key: string]: string }[];
+interface DataHeaders { [key:string]:string }[];
+
+class CreateTableByJson {
+    table: HTMLTableElement;
+    data: object;
+    headers: DataHeaders;
+    
+    constructor(data:object, headers: DataHeaders, table?: HTMLTableElement) {
+        this.table = table ? table : document.createElement('table');
+        this.data = data;
+        this.headers = headers;
     }
 }
-let a: Circle = { kind: "circle", radius: 22 }
-console.log(getArea(a));
+const data_c1 = { data: "key", item: 25, teste: { kest: "Teste", id: 75 }, date: "2024-11-05" };
 
-function printToConsole(s: string) {
-    console.log(s);    
-}
-greeter(printToConsole);
+const headers_c1: DataHeaders = [{'data': "Chave de Valores"}, {'item.kest': "Teste"}, { 'date': 'Data' }];
 
-type GreetFunction = (a: string) => void;
-function greeter(fn:GreetFunction) {
-    fn("Hello World");
-}
+const c1: CreateTableByJson = new CreateTableByJson(data_c1, headers_c1, html_table);
 
-type DescribableFunction = {
-    description: string;
-    (someArg: number): boolean;
-};
 
-function doSomething(fn: DescribableFunction) {
-    console.log(fn.description + " returned" + fn(6));
-}
-
-function myFunc(someArg: number) {
-    return someArg > 3;
-}
-myFunc.description = "default description";
-
-doSomething(myFunc);
-
-class SomeObject {
-    some:string
-    constructor(s: string){
-        this.some = s;
-    }
-}
-
-type SomeConstructor = {
-    new (s: string): SomeObject;
-};
-
-function fn(ctor: SomeConstructor): SomeObject {
-    return new ctor("hello");
-}
-
-let x:SomeConstructor = SomeObject;
-
-console.log(fn(x).some);
-
-interface CallOrConstruct {
-    (n?: number): string;
-    new (s:string): Date;
-}
-
-function firstElement<T>(arr: T[]): T | undefined {
-    return arr[0];
-}
-
-const s = firstElement(["a", "b", "c"]);
-const n = firstElement([1, 2, 3]);
-const u = firstElement([]);
-
-console.log(s, n, u);
-
-function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
-    return arr.map(func);
-}
-
-const parsed = map(["1", "2", "3"], (n) => parseInt(n));
-console.log(parsed);
-
-function longest<T extends { length: number }>(a:T, b:T) {
-    if (a.length >= b.length) {
-        return a;
-    } else {
-        return b;
-    }
-}
-const longerArray = longest([1, 2], [1, 2, 3]);
-const longerString = longest("alice", "bob");
-
-function myForEach(arr: any[], callback: (arg: any, index?: number) => void) {
-    for (let i = 0; i < arr.length; i++) {
-        callback(arr[i], i)
-    }
-}
-
-myForEach([1, 2, 3], (a) => console.log(a));
-myForEach([1, 2, 3], (a, i) => console.log(a, i));
-
-function makeDate(timestamp: number): Date;
-function makeDate(m: number, d: number, y: number): Date;
-function makeDate(mOrTimestamp: number, d?: number, y?: number): Date {
-    if (d !== undefined && y !== undefined) {
-        return new Date(y, mOrTimestamp, d);
-    } else {
-        return new Date(mOrTimestamp);
-    }
-}
-
-const d1 = makeDate(12345678);
-const d2 = makeDate(5, 5, 5);
-
-console.log(d1, d2);
-
-type User = {
-    id: number;
-
-    admin: boolean;
-    becomeAdmin: () => boolean
-}
-
-interface DB {
-    filterUsers(filter: (this: User) => boolean): User[];
-}
-
-function multiply(n: number, ...m: number[]) {
-    return m.map((x) => n * x);
-}
-
-const mult = multiply(10, 2, 3, 7);
-console.log(mult);
+console.log(c1);
