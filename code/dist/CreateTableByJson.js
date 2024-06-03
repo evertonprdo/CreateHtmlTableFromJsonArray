b0b129858b7bb27a7277f2f672ff287534a9f773
Everton Cezar do Prado Junior <ecpj.prado@gmail.com>
2024-06-03 12:08:50 -0300

    Type tests
    Ainda experimentando funções e conceitos, buscando maneiras eficientes de construir o projeto

--- code/dist/CreateTableByJson.js
+++ code/dist/CreateTableByJson.js
@@ -1,45 +1,113 @@
 "use strict";
-const html_table = document.createElement('table');
-const html_table_title = document.createElement('title');
-const html_table_caption = document.createElement('caption');
-const html_thead = document.createElement('thead');
-const html_tbody = document.createElement('tbody');
-const html_tfoot = document.createElement('tfoot');
-const html_tr = document.createElement('tr');
-const html_th = document.createElement('th');
-const html_td = document.createElement('td');
-function createTdRow(str) {
-    const tr = document.createElement('tr');
-    for (let index = 0; index < 7; index++) {
-        const td = document.createElement(str);
-        td.innerText = "Iteração: " + index;
-        tr.appendChild(td);
-    }
-    return tr;
-}
-for (let index = 0; index < 25; index++) {
-    html_tbody.appendChild(createTdRow('td'));
-}
-html_thead.append(createTdRow('th'));
-html_tfoot.append(createTdRow('th'));
-html_table_caption.innerText = "Teste de Descrição de Tabela";
-html_table.appendChild(html_table_caption);
-html_table.appendChild(html_thead);
-html_table.appendChild(html_tbody);
-html_table.appendChild(html_tfoot);
-document.body.appendChild(html_table);
-[];
+;
+;
 class CreateTableByJson {
     table;
     data;
     headers;
+    html_options;
+    fns;
     constructor(data, headers, table) {
-        this.table = table ? table : document.createElement('table');
+        this.table = table ? table : this.createTable();
         this.data = data;
         this.headers = headers;
+        this.html_options = {};
+        this.fns = {};
+    }
+    playFn(key, prop) {
+        return this.fns[key](prop ?? null);
+    }
+    addFunction(key, fn) {
+        this.fns[key] = fn;
+    }
+    HtmlOptionSet(opt) {
+        if (opt) {
+            for (const key in opt) {
+                this.html_options[key] = opt[key];
+            }
+        }
+        else {
+            throw new Error;
+        }
+    }
+    HtmlOptionDelete(opt) {
+        if (opt.length > 0) {
+            opt.forEach(key => {
+                delete this.html_options[key];
+            });
+        }
+    }
+    setHtmlAtribute(html, opts) {
+        for (const key in opts) {
+            html.setAttribute(key, opts[key]);
+        }
+    }
+    createCell(type, inner_text, option) {
+        const cell = document.createElement(type);
+        if (option) {
+            option(cell);
+        }
+        cell.innerText = inner_text;
+        return cell;
+    }
+    createRow(str) {
+        const tr = document.createElement('tr');
+        for (let i = 1; i < 8; i++) {
+            const td = this.createCell(str, 'Iteração: ' + i);
+            tr.appendChild(td);
+        }
+        return tr;
+    }
+    createThead() {
+        const thead = document.createElement('thead');
+        thead.appendChild(this.createRow('th'));
+        return thead;
+    }
+    createTbody() {
+        const tbody = document.createElement('tbody');
+        for (let i = 0; i < 20; i++) {
+            tbody.appendChild(this.createRow('td'));
+        }
+        return tbody;
+    }
+    createTfoot() {
+        const tfoot = document.createElement('tfoot');
+        tfoot.appendChild(this.createRow('th'));
+        return tfoot;
+    }
+    createTable() {
+        const table = document.createElement('table');
+        table.appendChild(this.createThead());
+        table.appendChild(this.createTbody());
+        table.appendChild(this.createTfoot());
+        return table;
     }
 }
 const data_c1 = { data: "key", item: 25, teste: { kest: "Teste", id: 75 }, date: "2024-11-05" };
-const headers_c1 = [{ 'data': "Chave de Valores" }, { 'item.kest': "Teste" }, { 'date': 'Data' }];
-const c1 = new CreateTableByJson(data_c1, headers_c1, html_table);
-console.log(c1);
+const headers_c1 = { 'data': 'Teste', 'item.kest': 'Testando Aninhado', 'date': 'Data da Compra' };
+const c1 = new CreateTableByJson(data_c1, headers_c1);
+document.body.appendChild(c1.table);
+c1.setHtmlAtribute(c1.table, { 'data-columref': 'Teste', 'class': 'Classe-Teste' });
+c1.HtmlOptionSet({ 'Header.test': 'Oloi', 'Sabato': 'Teste', 'Teste.teste': 'De Teste que testa os teste' });
+c1.HtmlOptionDelete([]);
+function nomeada() {
+    console.log("nomeada");
+}
+function teste2() {
+    console.log("teste2");
+}
+function norm() {
+    let c = c1.createCell("td", 'Sera que esse tipo de coisa é realmente util? Provavel, mas talvez não desse jeito');
+    return c;
+}
+function fnAtribuicao(c) {
+    const tr = c.createRow('td');
+    tr.style.color = 'red';
+    console.log(tr);
+}
+c1.addFunction('chave', nomeada);
+c1.addFunction('teste2', teste2);
+c1.addFunction('norm', norm);
+c1.addFunction('teste', fnAtribuicao);
+let c = c1.playFn('teste', (c1));
+console.log(c);
--- code/src/CreateTableByJson.ts
+++ code/src/CreateTableByJson.ts
@@ -1,60 +1,137 @@
-const html_table: HTMLTableElement = document.createElement('table');
-const html_table_title: HTMLTitleElement = document.createElement('title');
-const html_table_caption: HTMLTableCaptionElement = document.createElement('caption');
+interface HtmlOptionType {
+    [prop: string]: string;
+};
 
-const html_thead: HTMLTableSectionElement = document.createElement('thead');
-const html_tbody: HTMLTableSectionElement = document.createElement('tbody');
-const html_tfoot: HTMLTableSectionElement = document.createElement('tfoot');
-
-const html_tr: HTMLTableRowElement = document.createElement('tr');
-const html_th: HTMLTableCellElement = document.createElement('th');
-const html_td: HTMLTableCellElement = document.createElement('td');
-
-function createTdRow(str: "th" | "td"): HTMLTableRowElement {
-    const tr = document.createElement('tr');
-    for (let index = 0; index < 7; index++) {
-        const td = document.createElement(str);
-        td.innerText = "Iteração: " + index;
-        tr.appendChild(td);
-    }
-    return tr;
-}
-
-for (let index = 0; index < 25; index++) {
-    html_tbody.appendChild(createTdRow('td'));
-}
-
-html_thead.append(createTdRow('th'));
-html_tfoot.append(createTdRow('th'));
-
-html_table_caption.innerText = "Teste de Descrição de Tabela";
-
-html_table.appendChild(html_table_caption);
-html_table.appendChild(html_thead);
-html_table.appendChild(html_tbody);
-html_table.appendChild(html_tfoot);
-
-document.body.appendChild(html_table);
-
-//type DataHeaders = { [key: string]: string }[];
-interface DataHeaders { [key:string]:string }[];
+interface HeaderOptionType {
+    [prop: string | number]: any;
+};
 
 class CreateTableByJson {
     table: HTMLTableElement;
     data: object;
-    headers: DataHeaders;
+    headers: HeaderOptionType;
+    html_options: HtmlOptionType;
+    fns: {[prop: string]: (prop?:any) => any};
     
-    constructor(data:object, headers: DataHeaders, table?: HTMLTableElement) {
-        this.table = table ? table : document.createElement('table');
+    constructor(data:object, headers: HeaderOptionType, table?: HTMLTableElement) {
+        this.table = table ? table : this.createTable();
         this.data = data;
         this.headers = headers;
+        this.html_options = {} as HtmlOptionType;
+        this.fns = {};
+    }
+
+    playFn(key: string, prop?: any): any {
+        return this.fns[key](prop?? null);
+    }
+
+    addFunction(key: string, fn: (prop:any) => any): void {
+        this.fns[key] = fn;
+    }
+
+    HtmlOptionSet(opt: HtmlOptionType): void {
+        if(opt) {
+            for (const key in opt) {
+                this.html_options[key] = opt[key];
+            }
+        } else {
+            throw new Error;
+        }
+    }
+
+    HtmlOptionDelete(opt: string[]): void {
+        if (opt.length > 0) {
+            opt.forEach(key => {
+                delete this.html_options[key]
+            })
+        }
+    }
+
+    setHtmlAtribute(html: HTMLElement, opts: HtmlOptionType): void {
+        for (const key in opts) {
+            html.setAttribute(key, opts[key] as string)
+        }
+    }
+
+    createCell(type: "th" | "td", inner_text: string, option?: (cell: HTMLTableCellElement) => void): HTMLTableCellElement {
+        const cell = document.createElement(type);
+        if (option) {
+            option(cell);
+        }
+        cell.innerText = inner_text;
+        return cell;
+    }
+
+    createRow(str: "th" | "td"): HTMLTableRowElement {
+        const tr = document.createElement('tr');
+        for (let i = 1; i < 8; i++) {
+            const td = this.createCell(str, 'Iteração: ' + i);
+            tr.appendChild(td);
+        }
+        return tr;
+    }
+
+    createThead(): HTMLTableSectionElement {
+        const thead = document.createElement('thead');
+        thead.appendChild(this.createRow('th'));
+        return thead;
+    }
+
+    createTbody(): HTMLTableSectionElement {
+        const tbody = document.createElement('tbody');
+        for (let i = 0; i < 20; i++) {
+            tbody.appendChild(this.createRow('td'));
+        }
+        return tbody;
+    }
+
+    createTfoot(): HTMLTableSectionElement {
+        const tfoot = document.createElement('tfoot');
+        tfoot.appendChild(this.createRow('th'));
+        return tfoot;
+    }
+
+    createTable(): HTMLTableElement {
+        const table = document.createElement('table');
+        table.appendChild(this.createThead());
+        table.appendChild(this.createTbody());
+        table.appendChild(this.createTfoot());
+        return table;
     }
 }
 const data_c1 = { data: "key", item: 25, teste: { kest: "Teste", id: 75 }, date: "2024-11-05" };
+const headers_c1: HeaderOptionType = {'data': 'Teste', 'item.kest': 'Testando Aninhado', 'date': 'Data da Compra'};
 
-const headers_c1: DataHeaders = [{'data': "Chave de Valores"}, {'item.kest': "Teste"}, { 'date': 'Data' }];
+const c1: CreateTableByJson = new CreateTableByJson(data_c1, headers_c1);
+document.body.appendChild(c1.table);
 
-const c1: CreateTableByJson = new CreateTableByJson(data_c1, headers_c1, html_table);
+c1.setHtmlAtribute(c1.table, { 'data-columref': 'Teste', 'class': 'Classe-Teste' });
 
+c1.HtmlOptionSet({'Header.test': 'Oloi', 'Sabato': 'Teste', 'Teste.teste': 'De Teste que testa os teste'});
+c1.HtmlOptionDelete([]);
 
-console.log(c1);
+function nomeada(): void {
+    console.log("nomeada");
+}
+function teste2(): void {
+    console.log("teste2");
+}
+function norm(): any {
+    let c = c1.createCell("td", 'Sera que esse tipo de coisa é realmente util? Provavel, mas talvez não desse jeito')
+    return c;
+}
+
+function fnAtribuicao(c: CreateTableByJson) {
+    const tr = c.createRow('td');
+    tr.style.color = 'red';
+    console.log(tr);
+}
+
+c1.addFunction('chave', nomeada);
+c1.addFunction('teste2', teste2);
+c1.addFunction('norm', norm);
+c1.addFunction('teste', fnAtribuicao);
+
+let c = c1.playFn('teste', (c1));
+console.log(c);
+// É provavel que faz sentido atribuir funções a um objeto, porém suspeito que apenas se usar os atributos internos como parametro fnCustomEditProp(c:string), playCustomFn(key: sting) { call this.customEditPropFn[key](this.c) } inside obj, e chamar play por fora com c1.playCustomFn('String'); Caso contrário parece ser apenas atribuições de void fns ao objto para ser excutado em lote, talvez seja util por algum motivo, mas provavelmente seria o mesmo que executar as funções em um lote de funções por fora mesmo;
